4c5e0adfecf8fa9d09c6c8cfe3227563753e419c
Mimoosa <monami.saeki@gmail.com>
2026-03-20 11:38:44 +0200

    Add WeatherForecast project files as normal folder

---
+++ WetherForecast/WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Monami Kirjavainen on 20.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
